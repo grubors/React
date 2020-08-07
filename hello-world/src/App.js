872d417ff954c3a7c3e0571c3d5b612741436244
grubors <grubor.strahinja@gmail.com>
2020-08-07 22:53:29 +0200

    State added

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
+      <Message />
      {/*<Greet></Greet>    ctrl c k */}
       {/* <Welcome /> */}
       <Greet name="Bruce" heroName="Batman">
